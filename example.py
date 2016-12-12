da6d88a420b380815ad8cf0aa12e97669828caa2
Michael Washington <mike@gtwebsystems.com>
2016-12-11 20:33:43 -0500

    Update pyspark

--- example.py
+++ example.py
@@ -1,30 +1,30 @@
 import sys
 import pyspark
+#from random import random
+import random as ran
+import string
 
-zookeeper = sys.argv[1]
-hadoop_ip = sys.argv[2]
+master = sys.argv[1]
 
-print(zookeeper)
-print(hadoop_ip)
-
-src = 'hdfs://{}:8020/sample.txt'.format(hadoop_ip)
 conf = pyspark.SparkConf()
-conf.setMaster('mesos://zk://{}/mesos'.format(zookeeper))
+conf.setMaster("mesos://{}:5050".format(master))
 conf.setAppName('my_test_app')
-conf.set("spark.cores.max", "2")
-conf.set("spark.executor.memory", "2g")
-conf.set('spark.mesos.extra.cores', '1')
+conf.set('spark.mesos.coarse', 'true')
+conf.set('spark.executor.uri', 'http://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz')
 
-print("binary")
-# this must be a _prebuilt_ spark archive, i.e. a spark binary package
-# you can build it and host it yourself if you like.
-conf.set('spark.executor.uri', 'spark-1.5.0-bin-hadoop2.6.tgz')
-
+array = []
 sc = pyspark.SparkContext(conf=conf)
+num_of_words = 10000
 
-lines = sc.textFile(src)
-print("HEY")
-words = lines.flatMap(lambda x: x.split(' '))
-word_count = (words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x+y))
-print("DONE")
-print(word_count.collect())
+for i in range(num_of_words):
+    random = ''.join([ran.choice(string.ascii_letters + string.digits) for n in xrange(1)])
+    array.append(random)
+
+lines = sc.parallelize(array) 
+wordcounts = lines.map( lambda x: x.replace(',',' ').replace('.',' ').replace('-',' ').lower()) \
+        .flatMap(lambda x: x.split()) \
+        .map(lambda x: (x, 1)) \
+        .reduceByKey(lambda x,y:x+y) \
+        .map(lambda x:(x[1],x[0])) \
+        .sortByKey(True) 
+print wordcounts.take(50)
